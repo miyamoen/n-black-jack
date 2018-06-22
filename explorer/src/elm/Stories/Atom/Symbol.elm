c2af134cfd643e025f0cd036279b4dd293019d65
miyamoen <akihito.biblioteca@gmail.com>
2018-06-22 21:06:13 +0900

    Symbol List except Cards

---
+++ explorer/src/elm/Stories/Atom/Symbol.elm
@@ -0,0 +1,31 @@
+module Stories.Atom.PlainChip exposing (viewStories)
+
+import Element exposing (Element)
+import Rocket exposing ((=>))
+import Stories.Element exposing (toHtml, toHtmlWithSymbol, toMain)
+import UIExplorer exposing (renderStories)
+import View.Symbol as Symbol exposing (Symbol(..))
+
+
+type alias Config number =
+    { width : number, height : number, symbol : Symbol }
+
+
+view : Config number -> Element style variation msg
+view { width, height, symbol } =
+    Symbol.element width height symbol
+
+
+stories : List ( String, Config number )
+stories =
+    [ "Chip" => Config 100 100 Chip
+    , "SimpleChip" => Config 100 100 SimpleChip
+    ]
+
+
+viewStories =
+    renderStories (view >> toHtmlWithSymbol) stories
+
+
+main =
+    toMain viewStories
