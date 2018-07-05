195c1931c07f1f1fc639e7ac1de0dd0209e76a04
miyamoen <akihito.biblioteca@gmail.com>
2018-07-05 22:12:27 +0900

    Bet icon

---
+++ explorer/src/elm/Stories/Atom/Icon/Bet.elm
@@ -0,0 +1,28 @@
+module Stories.Atom.Icon.Bet exposing (viewStories)
+
+import Color.Pallet exposing (..)
+import Rocket exposing ((=>))
+import Stories.Utils exposing (fromStories, toHtml)
+import Types.Styles.Text exposing (Size(..))
+import UIExplorer exposing (renderStories)
+import View.Atom.Icon.Bet exposing (Config, view)
+
+
+stories : List ( String, Config {} )
+stories =
+    [ "Green" => { pallet = Pallet Green Lighten1, size = Medium }
+    , "Grey" => { pallet = Pallet Mono Lighten1, size = Medium }
+    , "Tiny" => { pallet = Pallet Red Darken2, size = Tiny }
+    , "Small" => { pallet = Pallet Blue Darken1, size = Small }
+    , "Normal" => { pallet = Pallet Orange Main, size = Normal }
+    , "Large" => { pallet = White, size = Large }
+    , "Huge" => { pallet = Black, size = Huge }
+    ]
+
+
+viewStories =
+    renderStories (view >> toHtml) stories
+
+
+main =
+    fromStories viewStories
---
+++ src/elm/View/Atom/Icon/Bet.elm
@@ -0,0 +1,47 @@
+module View.Atom.Icon.Bet exposing (Config, view)
+
+import Color.Pallet exposing (Pallet)
+import Element exposing (..)
+import Element.Attributes exposing (vary)
+import FontAwesome as FA exposing (..)
+import FontAwesome.Layers exposing (..)
+import Html.Keyed
+import Rocket exposing ((=>))
+import Types.Styles as Styles exposing (..)
+import Types.Styles.Text as Text exposing (Size(Medium), Variation(SizeVar))
+
+
+type alias Config a =
+    { a
+        | pallet : Pallet
+        , size : Text.Size
+    }
+
+
+view : Config a -> RootElement msg
+view { pallet, size } =
+    el Text [ vary (PalletVar pallet) True, vary (TextVar <| SizeVar size) True ] <|
+        html <|
+            Html.Keyed.node "div"
+                []
+                [ toString "bet-icon"
+                    => layers
+                        [ IconLayer FA.coins
+                            Solid
+                            [ Transform
+                                [ ShiftLeft 4
+                                ]
+                            ]
+                            []
+                        , IconLayer FA.levelUpAlt
+                            Solid
+                            [ Transform
+                                [ ShiftRight 6.5
+                                , ShiftUp 4
+                                , Grow 3
+                                ]
+                            ]
+                            []
+                        ]
+                        [ LayerHasFixedWidth ]
+                ]
