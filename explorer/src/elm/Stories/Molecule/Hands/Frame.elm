8875ac742e9fbac3503ba3bf88a90e7094252658
miyamoen <akihito.biblioteca@gmail.com>
2018-07-12 05:57:00 +0900

    Hands Frame

---
+++ explorer/src/elm/Stories/Molecule/Hands/Frame.elm
@@ -0,0 +1,33 @@
+module Stories.Molecule.Hands.Frame exposing (viewStories)
+
+import PushableData exposing (PushableData(..))
+import Rocket exposing ((=>))
+import Stories.Utils exposing (fromStories, toHtmlWithSymbol)
+import Types exposing (..)
+import Types.Firebase as Firebase
+import UIExplorer exposing (renderStories)
+import View.Molecule.Hands.Frame exposing (Data, view)
+
+
+data : PushableData Action Firebase.Error -> Data {}
+data action =
+    { action = action, cards = [ Card Spade Two Up, Card Spade Five Up ] }
+
+
+stories : List ( String, Data {} )
+stories =
+    [ "NotSelected" => data NotSelected
+    , "NotActive" => data NotActive
+    , "PushingHit" => data (Pushing Hit)
+    , "PushingStand" => data (Pushing Stand)
+    , "PushedHit" => data (Pushed Hit)
+    , "PushedStand" => data (Pushed Stand)
+    ]
+
+
+viewStories =
+    renderStories (view >> toHtmlWithSymbol) stories
+
+
+main =
+    fromStories viewStories
---
+++ src/elm/View/Molecule/Hands/Frame.elm
@@ -0,0 +1,94 @@
+module View.Molecule.Hands.Frame exposing (Data, view)
+
+import Color.Pallet exposing (..)
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import PushableData exposing (PushableData(..))
+import Types exposing (Action(..), Card)
+import Types.Firebase as Firebase
+import Types.Styles exposing (..)
+import Types.Styles.Selector as Selector
+import Types.Styles.Text exposing (..)
+import View.Atom.Icon.Edit as EditIcon
+import View.Atom.Icon.Error as ErrorIcon
+import View.Atom.Icon.Loading as LoadingIcon
+import View.Atom.Selector as Selector
+import View.Atom.Text as Text
+import View.Atom.Text.Point as Point
+import View.Molecule.Hands.Cards as Cards
+
+
+type alias Data a =
+    { a
+        | cards : List Card
+        , action : PushableData Action Firebase.Error
+    }
+
+
+view : Data a -> RootElement msg
+view data =
+    column None
+        [ center
+        , spacing 5
+        ]
+        [ Cards.view data
+            |> onLeft [ el None [ moveLeft 5 ] <| icon data ]
+        , el None [ height <| px 50 ] <|
+            Point.view textConfig data.cards
+        ]
+
+
+icon : Data a -> RootElement msg
+icon { action } =
+    case action of
+        NotSelected ->
+            EditIcon.view iconConfig
+
+        Pushing action ->
+            column None
+                []
+                [ actionIcon Selector.Selected action
+                , LoadingIcon.view iconConfig
+                ]
+
+        PushFailure { failed } ->
+            ErrorIcon.view iconConfig
+
+        Pushed action ->
+            actionIcon Selector.Confirmed action
+
+        NotActive ->
+            empty
+
+
+textConfig : Text.Config {}
+textConfig =
+    { style = BoldItaric
+    , size = Medium
+    , line = OneLine
+    , align = Center
+    , pallet = Just <| Pallet Orange Lighten1
+    , onTable = True
+    }
+
+
+iconConfig : { size : Size }
+iconConfig =
+    { size = Normal }
+
+
+actionIcon : Selector.State -> Action -> RootElement msg
+actionIcon state action =
+    Selector.view
+        { hue =
+            case action of
+                Hit ->
+                    Blue
+
+                Stand ->
+                    Red
+        , size = Tiny
+        , boxSize = px 30
+        }
+        state
+        (String.left 1 <| toString action)
